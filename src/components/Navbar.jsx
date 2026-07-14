a5ba8fa101cd9d2ff264861931f370c66ad514b5
Subodh Shukla <SubodhShukla@gmail.com>
2026-07-14 21:55:52 +0800

    add "navbar"

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+    return (
+        <>
+            <nav>
+                <a href="#">Home</a> | 
+                <a href="#">About</a> | 
+                <a href="#">Contact</a> | 
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
